6d0bbd8de089e9a0a8938f52faa3dd60866c6293
Gabriel Dazzi <offkas146@gmail.com>
2024-10-14 15:57:27 -0300

    dsmakdsa

--- src/js/how.js
+++ src/js/how.js
@@ -1,33 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const titles = document.querySelectorAll("h1");
-    
-    titles.forEach(title => {
-        title.addEventListener("click", () => {
-            const content = title.nextElementSibling; // Pega o próximo conteudo
-            content.classList.toggle("hidden");
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-            // Alterna a seta
-            const arrow = title.querySelector(".arrow");
-            if (content.classList.contains("hidden")) {
-                arrow.textContent = "ᐅ";
-            } else {
-                arrow.textContent = "ᐁ";
-            }
-        });
-    });
-});
-
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('nav ul');
-const body = document.body;
-
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const navMenu = document.querySelector('nav ul');
-
-    hamburger.addEventListener('click', function () {
-        hamburger.classList.toggle('toggle');
-        navMenu.classList.toggle('nav-active');
-    });
-});
-
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
