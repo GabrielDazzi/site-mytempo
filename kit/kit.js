e14c9f598d37171b1510a750fe66517cb6536d6e
GabrielDazzi <offkas146@gmail.com>
2024-11-13 08:19:27 -0300

    m

--- kit/kit.js
+++ kit/kit.js
@@ -2,17 +2,17 @@ function abrirImagem(src) {
     document.getElementById('imagem-grande').src = src;
     document.getElementById('modal').style.display = 'block';
     document.body.classList.add('modal-open');
-  
+
     const img = document.getElementById('imagem-grande');
     const textModal = document.querySelectorAll('.text-modal, .text-modal-2');
-  
-  }
-  
-  function fecharImagem() {
+
+}
+
+function fecharImagem() {
     document.getElementById('modal').style.display = 'none';
     document.body.classList.remove('modal-open');
-  
+
     const img = document.getElementById('imagem-grande');
     const textModal = document.querySelectorAll('.text-modal, .text-modal-2');
     textModal.forEach(el => el.style.visibility = 'visible');
-  }
+}
