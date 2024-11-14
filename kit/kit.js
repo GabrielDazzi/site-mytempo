8fc62be7787ff5f66207f4c6309841b2114b928f
GabrielDazzi <offkas146@gmail.com>
2024-11-14 09:50:05 -0300

    m

--- kit/kit.js
+++ kit/kit.js
@@ -16,3 +16,24 @@ function fecharImagem() {
     const textModal = document.querySelectorAll('.text-modal, .text-modal-2');
     textModal.forEach(el => el.style.visibility = 'visible');
 }
+
+window.addEventListener('load', applyFadeInAnimation); // Aplica ao carregar a página
+
+// Função para aplicar a animação de fade-in
+function applyFadeInAnimation() {
+    const elementsToFade = document.querySelectorAll('.number-section, .instruction-section, .chip-section, .kit-contents, .real-illustration, .load');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target); // Para observar apenas uma vez
+            }
+        });
+    });
+
+    elementsToFade.forEach(el => {
+        el.classList.add('fade-in'); // Adiciona a classe fade-in inicialmente
+        observer.observe(el); // Observa cada elemento
+    });
+}
