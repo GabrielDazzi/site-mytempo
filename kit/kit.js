a19f603bc2650619df3e41600c7ebdabe007c6e1
GabrielDazzi <offkas146@gmail.com>
2024-11-19 13:42:30 -0300

    m

--- kit/kit.js
+++ kit/kit.js
@@ -19,7 +19,7 @@ window.addEventListener('load', applyFadeInAnimation); // Aplica ao carregar a p
 // Função para aplicar a animação de fade-in
 function applyFadeInAnimation() {
     const elementsToFade = document.querySelectorAll('.number-section, .instruction-section, .chip-section, .kit-contents, .real-illustration, .load');
-
+  
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
