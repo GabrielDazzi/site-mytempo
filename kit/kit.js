c2fbbacfeaf9ff05f52042ac58cd166a2afa43e4
GabrielDazzi <offkas146@gmail.com>
2024-11-22 09:30:10 -0300

    m

--- kit/kit.js
+++ kit/kit.js
@@ -73,7 +73,7 @@ function observeElementsForFadeIn() {
 // // Função para aplicar a animação de fade-in
 // function applyFadeInAnimation() {
 //     const elementsToFade = document.querySelectorAll('.number-section, .instruction-section, .chip-section, .kit-contents, .real-illustration, .load');
-// ====================================================================================================================================================================================================
+// ===============================================================================================================================================================================
 //     const observer = new IntersectionObserver((entries, observer) => {
 //         entries.forEach(entry => {
 //             if (entry.isIntersecting) {
